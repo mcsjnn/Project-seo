4dfc5c7013db6efaf720d4bc0ae4d7b314380ebb
mcsjnn <sujenn.ancco@tecsup.edu.pe>
2025-06-16 12:12:32 -0500

    cambio en index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
-import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Mi Sitio Optimizado - Home</title>
-        <meta name="google-site-verification" content="yuhdwlhoXi-Uj7-EuK5aNdAiDQiUSiTQoJAP3VbZvuQ" />
-        <meta name="description" content="Aprende sobre optimización SEO y rendimiento en Next.js." />
-        <meta name="keywords" content="Next.js, SEO, optimización web" />
-        <meta property="og:title" content="Mi Sitio Optimizado" />
-        <meta property="og:description" content="Descubre técnicas avanzadas para mejorar tu web con Next.js." />
-        <meta property="og:image" content="/images/seo-image.png" />
-        <meta property="og:type" content="website" />
-      </Head>
-
-      <h1>Bienvenido a mi página optimizada</h1>
-      <p>Aprende cómo mejorar el rendimiento y SEO en Next.js.</p>
+      <h1>Bienvenido</h1>
+      <Image
+        src="/images/seo-image.jpg"
+        width={800}
+        height={400}
+        alt="Ejemplo de imagen optimizada"
+        priority
+      />
     </>
   );
 }
